cf0bc6c43e6fddd0a1393ea59ddac537a18ddc3d
Ajay <92861422+ajay09a@users.noreply.github.com>
2023-12-23 13:26:42 +0530

    filter array to display not completed item first

--- src/List.js
+++ src/List.js
@@ -9,7 +9,26 @@ function List(props) {
       };
   return (
     <div className="todo-list">
-        {props.todos.map((todo) => (
+        {/* {props.todos.map((todo) => (
+          <div
+            key={todo.id}
+            className={`todo-card ${todo.completed ? 'completed' : ''}`}
+            onClick={() => toggleTodo(todo.id)}
+          >
+            {todo.text}
+          </div>
+        ))} */}
+
+        {props.todos.filter(todo=>!todo.completed).map((todo) => (
+          <div
+            key={todo.id}
+            className={`todo-card ${todo.completed ? 'completed' : ''}`}
+            onClick={() => toggleTodo(todo.id)}
+          >
+            {todo.text}
+          </div>
+        ))}
+        {props.todos.filter(todo=>todo.completed).map((todo) => (
           <div
             key={todo.id}
             className={`todo-card ${todo.completed ? 'completed' : ''}`}
