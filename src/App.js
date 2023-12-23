11afc1f1420a4213e3b20497271183068d6d14e2
Ajay <92861422+ajay09a@users.noreply.github.com>
2023-12-23 12:02:50 +0530

    add input field and console the input item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { useState } from 'react';
 import './App.css';
+import List from './List';
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    console.log(input)
+  };
+
   return (
     <div className="App">
-      App
+      <h1>TODO LIST</h1>
+      <input
+          type="text"
+          placeholder="Add a todo..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyPress={(e) => e.key === 'Enter' && addTodo()}
+        />
+      <List/>
     </div>
   );
 }
